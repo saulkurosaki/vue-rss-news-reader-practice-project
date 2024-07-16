cf5e2a3951841cf367be64e4cdaa805e305fb46e
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 13:40:11 -0600

    feat(Sidebar): implement the handleClick function to call the feedStore loadSource function

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,12 +2,18 @@
 import { useFeedStore } from "../stores/store";
 
 const feedStore = useFeedStore();
+
+const handleClick = (source) => {
+  feedStore.loadSource(source);
+};
 </script>
 
 <template>
   <div class="sidebar">
     <div v-for="(source, index) in feedStore.sources" :key="index">
-      <a href="#">{{ source.name }}</a>
+      <a href="#" @click.prevent="() => handleClick(source)">{{
+        source.name
+      }}</a>
     </div>
   </div>
 </template>

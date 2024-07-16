4a45d2dbd1ae612230733a1eac4005f539c5b4b5
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 13:20:41 -0600

    feat(Posts): implement the Posts component initial layout

--- src/components/RSSReader.vue
+++ src/components/RSSReader.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from "./Header.vue";
 import Sidebar from "./Sidebar.vue";
+import Posts from "./Posts.vue";
 </script>
 
 <template>
   <div class="main-container">
     <Header />
     <Sidebar />
+    <Posts />
   </div>
 </template>
 

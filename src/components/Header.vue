ec4370a1a75b7cac0c1292664c672225310b8768
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 13:29:42 -0600

    feat(Header): implement the components initial structure along its handleSubmit function, the url ref and the feedStore call

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useFeedStore } from "@/stores/store";
 
-<template></template>
+const url = ref("");
+const feedStore = useFeedStore();
+
+const handleSubmit = () => {
+  if (!!url.value) {
+    feedStore.registerNewSource(url.value);
+  }
+};
+</script>
+
+<template>
+  <div class="header">
+    <form action="" @submit.prevent="handleSubmit">
+      <input type="url" required v-model="url" />
+      <button type="submit">Add Source</button>
+    </form>
+  </div>
+</template>
 
 <style scoped></style>

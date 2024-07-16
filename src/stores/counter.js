5f9f74713e13ac9e7f2e93b43c1fa9e5f9e806e9
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 13:13:31 -0600

    feat(store.js): initialize the pinia store manager along initializing the feedStore with its source reactive state

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,21 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+// EJEMPLO DE PINIA STATE MANAGER
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+// 1er parametro: Id del Store
+// 2do parametro: Arrow function con el objeto de configuración
+export const useCounterStore = defineStore("counter", () => {
+  // Estado de variable reactiva (ref o reactive)
+  const count = ref(0);
+
+  // Getters: funciones que van a computarizarse con cada actualización de estado
+  const doubleCount = computed(() => count.value * 2);
+
+  // Actions: funciones que modifican directamente el estado
   function increment() {
-    count.value++
+    count.value++;
   }
 
-  return { count, doubleCount, increment }
-})
+  return { count, doubleCount, increment };
+});
---
+++ src/stores/store.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import { reactive } from "vue";
+
+export const useFeedStore = defineStore("feedStore", () => {
+  // Estados (información de los RSS)
+  const source = reactive([
+    {
+      id: crypto.randomUUID(),
+      name: "Mozilla Hacks",
+      url: "https://hacks.mozilla.org/feed",
+    },
+  ]);
+
+  //   const current = reactive({
+  //     source: null,
+  //     items: null,
+  //   });
+
+  // Actions
+  //   async function loadSource(source) {}
+
+  //   async function registerNewSource(url) {}
+
+  return { source, current };
+});

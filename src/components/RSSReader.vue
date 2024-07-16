2a3ee0fb2694fb8039851d8ddad0f01535c0df10
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 13:19:56 -0600

    feat(Sidebar): implement the Sidebar component initial layout

--- src/components/RSSReader.vue
+++ src/components/RSSReader.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+import Header from "./Header.vue";
+import Sidebar from "./Sidebar.vue";
+</script>
 
 <template>
-  <div class="main-container"></div>
+  <div class="main-container">
+    <Header />
+    <Sidebar />
+  </div>
 </template>
 
 <style scoped></style>

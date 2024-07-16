7436c83b9ee53723fdd33e033f9b4e9d144a014c
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 13:45:22 -0600

    feat(Posts): implement the components initial structure along the feedStore call and the iteration for each post within the feedStore items

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import { useFeedStore } from "@/stores/store";
 
-<template></template>
+const feedStore = useFeedStore();
+</script>
+
+<template>
+  <div class="main">
+    <h1 v-if="feedStore.current.source">{{ feedStore.current.source.name }}</h1>
+
+    <details v-for="post in feedStore.current.items" :key="post.title">
+      <summary>{{ post.title }}</summary>
+      <div v-html="post.content"></div>
+    </details>
+  </div>
+</template>
 
 <style scoped></style>
